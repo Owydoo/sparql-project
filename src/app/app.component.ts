230179302db5eeafb60ed8923a04a3faaed3f0ac
Hugo <owydoo@gmail.com>
2022-03-01 20:21:07 +0100

    first sparql request linked to button and displaying link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sparql-project';
+  sparkqlData: string = 'hello sparql';
+  constructor(private http: HttpClient) {}
+
+  doRequest = () => {
+    console.log('hello');
+
+    let headers: any = new HttpHeaders({
+      'Content-type': 'application/x-www-form-urlencoded',
+    });
+
+    let params = new HttpParams();
+    params = params.append(
+      'query',
+      'SELECT DISTINCT ?picture ?country WHERE { ?country dbo:capital ?capital. ?capital rdfs:label "Beijing" @en. ?capital foaf:depiction ?picture  } LIMIT 15'
+    );
+    params = params.append('format', 'json');
+
+    interface ResponseObject {
+
+    }
+
+    this.http
+      .get('http://dbpedia.org/sparql', { headers: headers, params: params })
+      .subscribe((data:any) => {
+        // console.log("data object : ",data.results.bindings);
+        console.log("data object", data);
+        console.log("data test", data.results.bindings[0].picture.value);
+        this.sparkqlData = data.results.bindings[0].picture.value;
+        console.log("data en string : ", data.toString());
+        
+        // this.sparkqlData = data.results.toString(); // 3
+      });
+    console.log(this.sparkqlData); // 2
+  };
 }
